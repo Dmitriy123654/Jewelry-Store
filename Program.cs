e44773fa47d9cb2d343e7a1c9e3f881b6c53517b
Dmitriy Ermak <jamayka.10@mail.ru>
2023-09-11 00:20:54 +0300

    The basic structure of the site (mvc) was created. Connection to the database has been made.

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ namespace WebApp
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-            
+
             app.Run();
         }
         
