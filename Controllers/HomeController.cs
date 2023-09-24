7973d44f947a54b268b5a5caefbcc65b7e88d572
Dmitriy Ermak <jamayka.10@mail.ru>
2023-09-24 06:19:37 +0300

    feat: add a Home page UI and sorts Products by categories
    The UI of the main page has been added, namely the list of products(with
    a picture and their fields, and a button about the detailed
    characteristics of the product). A line has also been added where we can
    select the desired category and show us the products of this category,
    or all Products

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using WebApp.Data;
 using WebApp.Models;
+using WebApp.Models.ViewModels;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext db;
+        private static HomeVM? homeVMAllProducts;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext _db)
         {
             _logger = logger;
-        }
+            db = _db;
+            homeVMAllProducts = new HomeVM()
+            {
+                Products = db.Products.Include(u => u.Category)
+                                      .Include(u => u.ApplicationType),
+                Categories = db.Categories
 
+            };
+        }
+        //Get
         public IActionResult Index()
         {
-            return View();
+            
+            return View(homeVMAllProducts);
+        }
+        [HttpPost]
+        public IActionResult IndexPost(string? category, string JsonHomeVM="")
+        {
+            var homeVM = JsonConvert.DeserializeObject<HomeVM>(JsonHomeVM);
+            if (category == "all")
+            {
+                return View("Index", homeVMAllProducts);
+            }
+            else
+            {
+                var viewModel = new HomeVM
+                {
+                    Products = db.Products.Where(p => p.Category.Name == category)
+                                                                .Include(p => p.Category)
+                                                                .Include(p => p.ApplicationType),
+                    Categories = homeVM.Categories
+            };
+                return View("Index", viewModel);
+            }
+
         }
 
         public IActionResult Privacy()
---
+++ Models/ViewModels/HomeVM.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
