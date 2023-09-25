17bd06fac665424f5e2c67f9375a0c4df56ce1f7
Dmitriy Ermak <jamayka.10@mail.ru>
2023-09-25 05:29:26 +0300

    feat: add UI shopping cart

---
+++ Controllers/CartController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
+using WebApp.Utility;
+
+namespace WebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public CartController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart)!.Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WebConstants.SessionCart)!;
+            }
+            List<int> productInCart = shoppingCartList.Select(x => x.ProductId).ToList();
+            IEnumerable<Product> productList = db.Products.Where(x => productInCart.Contains(x.ProductId));
+            return View(productList);
+        }
+    }
+}
